1825fd4f732d16b3023875d6c1df4169315d2fea
UttkarshKhanke <uttkarsh.khanke@gmail.com>
2023-11-24 15:55:09 +0530

    initial commmit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+
+@section('content')
+    <article>
+        <section>
+            <div>
+                <h2 class="font-bold text-3xl">this is test welcome heading</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos non modi iste deleniti dolor recusandae, ab, aliquam rerum dolorem perspiciatis mollitia? Sed, perferendis ut sapiente id quo vitae quis animi.</p>
+            </div>
+        </section>
+    </article>
+@endsection
